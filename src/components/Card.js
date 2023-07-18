643e63fa396fa3007aa3c22c2e1d863f459d3151
borove4ik <borove4ik@gmail.com>
2023-07-19 00:04:40 +0300

    исправлен функционал лайка

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,35 +9,36 @@ export class Card {
   deleteCard = () => {
     this._element.remove();
     this._element = null;
-  }
+  };
 
-  _setEventListeners(likeButton, galleryTrash) {
-    likeButton.addEventListener("click", (evt) => {
-      evt.currentTarget.classList.toggle("gallery__like_active");
-    });
-    galleryTrash.addEventListener('click', this.deleteCard);
-    this.photo.addEventListener('click', this._handleCardClick)
+_handleLikeClick(evt) {
+  evt.currentTarget.classList.toggle("gallery__like_active");
+}
+
+  _setEventListeners() {
+    this._likeButton.addEventListener("click", (evt) => this._handleLikeClick(evt));
+    this._galleryTrash.addEventListener("click", this.deleteCard);
+    this.photo.addEventListener("click", this._handleCardClick);
   }
 
   _getTemplate() {
-    const cardElement = document.querySelector(this._templateSelector)
-    .content
-    .querySelector(".gallery__element")
-    .cloneNode(true);
+    const cardElement = document
+      .querySelector(this._templateSelector)
+      .content.querySelector(".gallery__element")
+      .cloneNode(true);
     return cardElement;
   }
 
   createCard() {
     this._element = this._getTemplate();
     this._likeButton = this._element.querySelector(".gallery__like");
-    this._galleryTrash = this._element.querySelector('.gallery__trash');
+    this._galleryTrash = this._element.querySelector(".gallery__trash");
     this._element.querySelector(".gallery__photo").src = this._link;
     this._element.querySelector(".gallery__photo").alt = this._name;
     this.photo = this._element.querySelector(".gallery__photo");
-    this._element.querySelector(".gallery__element-description").textContent = this._name;
-    this._setEventListeners(this._likeButton, this._galleryTrash);
-    return this._element
+    this._element.querySelector(".gallery__element-description").textContent =
+      this._name;
+    this._setEventListeners();
+    return this._element;
   }
-  
 }
-
